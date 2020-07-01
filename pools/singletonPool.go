c424a2c2a09b8af15116e62a3f4d3859562e8955
Pavel Paroulek <pparoulek@frinx.io>
2020-07-01 16:02:59 +0200

    Fixing singleton pool

---
+++ pools/singletonPool.go
@@ -0,0 +1,41 @@
+package pools
+
+import (
+	"context"
+	"github.com/marosmars/resourceManager/ent"
+)
+
+// NewSingletonPool creates a brand new pool allocating DB entities in the process
+func NewSingletonPool(
+	ctx context.Context,
+	client *ent.Client,
+	resourceType *ent.ResourceType,
+	propertyValues RawResourceProps,
+	poolName string) (Pool, error) {
+
+	pool, err := newPoolInner(ctx, client, resourceType, []RawResourceProps{propertyValues}, poolName)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &SingletonPool{SetPool{pool, ctx, client}}, nil
+}
+
+func (pool SingletonPool) ClaimResource() (*ent.Resource, error) {
+	return pool.queryUnclaimedResourceEager()
+}
+
+func (pool SingletonPool) FreeResource(raw RawResourceProps) error {
+	return nil
+}
+
+func (pool SingletonPool) QueryResource(raw RawResourceProps) (* ent.Resource, error) {
+	return pool.QueryResource(raw)
+}
+
+func (pool SingletonPool) QueryResources() (ent.Resources, error) {
+	return pool.QueryResources()
+}
+
+
